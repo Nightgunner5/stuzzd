8e3ce6e2add8fb3db0a4dda2e1d039320c827452
Ben L <nightgunner5@llamaslayers.net>
2012-07-26 17:10:05 -0500

    Fix typos

--- commands/validateregions.go
+++ commands/validateregions.go
@@ -1,6 +1,12 @@
 package main
 
-import ("os";"encoding/binary";"log";"strings")
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
 	dir, err := os.Open(os.Args[0])
@@ -23,21 +29,21 @@ func main() {
 			for z := 0; z < 32; z++ {
 				for x := 0; x < 32; x++ {
 					var location int32
-					binary.Read(region, binary.BigEndian, &location)
+					binary.Read(f, binary.BigEndian, &location)
 
-					offset := location>>8
-					sectorCount := int32(int8(location&0xFF)) // Get the sign bit to be a sign bit
+					offset := location >> 8
+					sectorCount := int32(int8(location & 0xFF)) // Get the sign bit to be a sign bit
 					if offset == 0 && sectorCount == 0 {
 						// No chunk here yet.
 					} else if sectorCount <= 0 {
 						log.Printf("In %s: Chunk(%d, %d) has zero or a negative number of sectors (%d)", file.Name(), x, z, sectorCount)
 					} else if offset < 2 {
 						log.Printf("In %s: Chunk(%d, %d) has an invalid offset (too low: %d)", file.Name(), x, z, offset)
-					} else if offset + sectorCoount > sectors {
+					} else if offset+sectorCount > sectors {
 						log.Printf("In %s: Chunk(%d, %d) has an invalid offset or sector count (goes past end of file: %d, %d)", file.Name(), x, z, offset, sectorCount)
 					} else {
 						chunkName := fmt.Sprintf("Chunk(%d, %d)", x, z)
-						for i := offset; i < offset + sectorCount; i++ {
+						for i := offset; i < offset+sectorCount; i++ {
 							if accountedFor[i] != "" {
 								log.Printf("In %s: %s claims offset %d, but the offset is already owned by %s", file.Name(), chunkName, i, accountedFor[i])
 							} else {
